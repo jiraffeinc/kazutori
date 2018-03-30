83b0783ded96df20205ef638db24bb26974789cc
yalab <rudeboyjet@gmail.com>
2018-03-30 18:28:38 +0900

    #count_up accept argument.

--- lib/kazutori/base.rb
+++ lib/kazutori/base.rb
@@ -8,8 +8,8 @@ module Kazutori
       @redis = Redis::Namespace.new solve_namespace(args), redis: Redis.new(url: ENV["REDIS_URL"])
     end
 
-    def count_up
-      @redis.incr(key_for(Time.zone.now)).to_i
+    def count_up(time=Time.zone.now)
+      @redis.incr(key_for(time)).to_i
     end
 
     private
@@ -18,7 +18,7 @@ module Kazutori
         if object.respond_to?(:to_global_id)
           object.to_global_id.to_s
         else
-          object.to_sgit
+          object.to_s
         end
       end.join(':')
     end
--- spec/kazutori/hourly_spec.rb
+++ spec/kazutori/hourly_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe Kazutori::Hourly do
       kazutori = Kazutori::Hourly.new
       expect(kazutori.count_up).to eq 1
       expect(kazutori.count_up).to eq 2
+      expect(kazutori.count_up(1.hours.ago)).to eq 1
+      expect(kazutori.count_up(1.hours.ago)).to eq 2
       kazutori = Kazutori::Hourly.new("other")
       expect(kazutori.count_up).to eq 1
       expect(kazutori.count_up).to eq 2
