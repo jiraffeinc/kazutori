ad3cefc5d4f98d889f665ca3c2cce7ae22e363c6
yalab <rudeboyjet@gmail.com>
2018-03-31 17:19:43 +0900

    Count add validations.

--- app/models/kazutori/count.rb
+++ app/models/kazutori/count.rb
@@ -1,4 +1,7 @@
 module Kazutori
-  class Count < ApplicationRecord
+  class Count < ActiveRecord::Base
+    belongs_to :countable, polymorphic: true
+    validates :count, presence: true, numericality: { only_integer: true, greater_than: 0 }
+    validates :hour, presence: true
   end
 end
--- spec/models/kazutori/count_spec.rb
+++ spec/models/kazutori/count_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 module Kazutori
   RSpec.describe Count, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    describe 'validations' do
+      it { is_expected.to validate_presence_of :count }
+      it { is_expected.to validate_numericality_of(:count).only_integer }
+      it { is_expected.to validate_numericality_of(:count).is_greater_than(0) }
+      it { is_expected.to validate_presence_of :hour }
+    end
   end
 end
